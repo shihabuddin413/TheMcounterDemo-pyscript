9fdd8a56078c2174acba63801b2b00d4a9c39fd5
shihabe uddin <shihab38900@gmail.com>
2022-06-05 18:23:40 +0000

    doing bullshit

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
-
-def getTodo(*args, **kwargs):
-    outputbox = Element('output')
-    inputBox = Element('todoTextInput')
-
-    data =  inputBox.value
-
-    console.log(Element)
-
-    outputbox.write(data)
-
 class Todo():
     def __init__(self):
         self.todos = []
+        self.warn_ui = """<div class="alert alert-warning alert-dismissible fade show" role="alert">
+            Please Enter Something Before Add
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>"""
 
     def add_todo(self, item):
-        if (item in self.todos):
-            return "This item exist"
-        self.todos.add(item)
+        if len(item.strip()) == 0:
+            alertBox = Element('alert')
+            alertBox.write(self.warn_ui)
+            return 
+
+        self.todos.append(item)
     
     def remove_todo(self, item):
         tmp = []
@@ -24,7 +20,21 @@ class Todo():
             if (i != item):
                 tmp.append(i)
         self.todos = tmp
+    
+    def render(self):
+        items = ''
+        for i in self.todos:
+            items += (f'<li pys  class="list-group-item">{i}</li>')
+        return items
+        
 
+todoHandler = Todo()
 
-# mTodoHandler = Todo()
+def getTodo(*args, **kwargs):
+    outputbox = Element('output')
+    inputBox = Element('todoTextInput')
+    data =  inputBox.value
+    todoHandler.add_todo(data)
+    items = todoHandler.render()
+    outputbox.write(items)
 
