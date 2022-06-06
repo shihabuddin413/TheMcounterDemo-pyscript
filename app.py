f5007f7a4c485f061764b6a399c9a175acf104ab
shihabe uddin <shihab38900@gmail.com>
2022-06-06 01:19:02 +0000

    cmmit

--- app.py
+++ app.py
@@ -1,40 +1,69 @@
+
+warn_ui = """
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        {}
+        <button 
+            type="button" 
+            class="btn-close" data-bs-dismiss="alert" 
+            aria-label="Close">
+        </button>
+    </div>"""
+
+
 class Todo():
     def __init__(self):
         self.todos = []
-        self.warn_ui = """<div class="alert alert-warning alert-dismissible fade show" role="alert">
-            Please Enter Something Before Add
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>"""
 
     def add_todo(self, item):
         if len(item.strip()) == 0:
-            alertBox = Element('alert')
-            alertBox.write(self.warn_ui)
+            alertBox.write(warn_ui.format("Please Enter Something Before Add"))
             return 
 
         self.todos.append(item)
     
-    def remove_todo(self, item):
+    def remove_todo(self, item_idx):
+        if (len(item_idx.strip())==0 or int(item_idx)-1 < len(self.todos)):
+            alertBox.write(warn_ui.format("Item is not found"))
+        item_idx = int(item_idx)
         tmp = []
-        for i in self.todos:
-            if (i != item):
-                tmp.append(i)
-        self.todos = tmp
+        item = self.todos.pop(item_idx-1)
+        console.log(f'{item} has been deleted')
+        return item
     
     def render(self):
+        #This will return a ui string
         items = ''
+        template_item = '<li class="list-group-item d-flex justify-content-between"><span>{}</span> <button pys-onClick="removeItem" class="rm-btn"  > | Erase</button> </li>'
+
         for i in self.todos:
-            items += (f'<li pys  class="list-group-item">{i}</li>')
+            items += (template_item.format(i))
         return items
         
 
 todoHandler = Todo()
+alertBox = Element('alert')
+outputbox = Element('output')
+inputBox = Element('todoTextInput')
+deleteBox =  Element('deleteIdxInput')
 
 def getTodo(*args, **kwargs):
-    outputbox = Element('output')
-    inputBox = Element('todoTextInput')
     data =  inputBox.value
     todoHandler.add_todo(data)
     items = todoHandler.render()
     outputbox.write(items)
+
+def removeItem(*args, **kwargs):
+    items = todoHandler.render()
+    if len(items.strip()) == 0:
+        alertBox.write(warn_ui.format("Nothing Found To Delete"))
+        return
+    deleteIdx =deleteBox.value
+    getDeletedItem = todoHandler.remove_todo(deleteIdx)
+    items = todoHandler.render()
+    alertBox.write(warn_ui.format(f'Items <strong>{getDeletedItem}</strong> Has Been Deleted'))
+    outputbox.write(items)
+
+
+
+
 
